e66539037f0ef5f83ef57cf45300654421761187
RudzaniMatidze <rdzmatidze@gmail.com>
2023-11-14 05:01:33 -0800

    main.h updated

--- main.h
+++ main.h
@@ -4,10 +4,13 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int _printf(const char *format, ...);
 int _ptchr(char cr);
 int _putss(char *s);
 int print_rot13(va_list argmnt);
+int print_integers(va_list args);
+int print_decimals(va_list args);
 
 #endif
