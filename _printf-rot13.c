b86a8def4aa5e69677d0021c701f551106bf1a36
RudzaniMatidze <rdzmatidze@gmail.com>
2023-11-14 04:49:04 -0800

    R: prints the rot13'ed string

--- _printf-rot13.c
+++ _printf-rot13.c
@@ -1,20 +1,26 @@
 #include "main.h"
 /**
- * _print_rot13 - printf string to rot13
+ * print_rot13 - printf string to rot13
  * @argmnt: argument string
  * Return: number
- *
  */
-int _print_rot13(va_list argmnt)
+int print_rot13(va_list argmnt)
 {
 	int a, b, number = 0;
 	int k = 0;
 	char *s = va_arg(argmnt, char*);
-	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-	char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+		'I', 'J', 'K', 'L', 'M'};
+	char beta[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+		'V', 'W', 'X', 'Y', 'Z'};
 
 	if (s == NULL)
 		s = "(null)";
+
 	for (a = 0; s[a]; a++)
 	{
 		k = 0;
