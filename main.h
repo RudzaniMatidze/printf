ca4373dbd5239808b9660cf61db14649aab4acc8
Bubble-Bytes <evelynboora@gmail.com>
2023-11-14 10:44:37 -0800

    Try

--- main.h
+++ main.h
@@ -13,11 +13,6 @@ int _putss(char *s);
 int print_rot13(va_list argmnt);
 int print_integers(va_list args);
 int print_decimals(va_list args);
-int print_binary(va_list b);
-int print_percent(va_list args);
-int print_address(va_list args);
-int print_rev(va_list args);
-int print_string(va_list args);
-int print_char(va_list args);
+int print_binry(va_list b);
 
 #endif
